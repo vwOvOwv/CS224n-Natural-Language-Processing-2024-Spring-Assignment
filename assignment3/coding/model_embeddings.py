0d3c367fd447a9cf847e61c61c1d8b9f0ff1352e
vwOvOwv <1271440868@qq.com>
2026-03-05 19:59:20 +0800

    fix: correct embedding initialization for source and target languages

--- assignment3/coding/model_embeddings.py
+++ assignment3/coding/model_embeddings.py
@@ -52,8 +52,8 @@ class ModelEmbeddings(nn.Module):
         ### Use the following docs to properly initialize these variables:
         ###     Embedding Layer:
         ###         https://pytorch.org/docs/stable/generated/torch.nn.Embedding.html
-        self.source = nn.Embedding(len(vocab.src) + 1, embed_size)  # +1 for padding token
-        self.target = nn.Embedding(len(vocab.tgt) + 1, embed_size)
+        self.source = nn.Embedding(len(vocab.src), embed_size, padding_idx=src_pad_token_idx)
+        self.target = nn.Embedding(len(vocab.tgt), embed_size, padding_idx=tgt_pad_token_idx)
         ### END YOUR CODE
 
 
